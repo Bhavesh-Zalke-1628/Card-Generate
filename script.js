9e1440273b76014db384103bc5d54d51ed2a2e18
Bhavesh Zalke <zalkebhavesh@gmail.com>
2023-10-01 19:42:23 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const cardParent = document.getElementById("body")
+async function getData() {
+    try {
+        const res = await fetch('https://fakestoreapi.com/products')
+        const data = await res.json()
+        renderData(data)
+    } catch (error) {
+        alert("Error occured")
+    }
+}
+function renderData(data) {
+    data.forEach(element => {
+        const card = document.createElement("div")
+        card.className = "card"
+        card.innerHTML = `
+        <div id="img-div">
+        <img src="${element.image}" alt="">
+        </div>
+        <div id="text">
+            <h1>${element.title}</h1>
+            <p>${element.description}</p>
+            <button id="price">${element.price}</button>
+            </div>`
+        cardParent.appendChild(card);
+    });
+}
+getData()
